cfe5b6609958c12e00d3c012e18b633f5d8548a1
MarkSian <merksianipar@gmail.com>
2025-08-15 20:49:29 -0600

    Login Page
    Functionally Complete will need to style later

--- AniGate-FE/src/pages/LoginPage.tsx
+++ AniGate-FE/src/pages/LoginPage.tsx
@@ -1,12 +1,70 @@
+import axiosInstance from '../api/connector';
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 
-const LoginPage = () => {
+const LoginPage: React.FunctionComponent = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    
+    const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            await axiosInstance.post('/auth/login', { username, password}, { withCredentials: true  });
+            navigate('/content'); 
+                
+        } catch(err) {
+            alert('Login failed. Please check your credentials.');
+            console.error(err);
+        }
+    };
+
+    
+
 
     return (
-        <div className='text-secondary'>
-            <h1>Login Page</h1>
-            <p>This is the Login Page</p>
-        </div>
+       <>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-base-200">
+                <h1 className="text-4xl font-bold text-primary mb-12 mt-4">AniVault</h1>
+                <div className="card w-96 bg-base-100 shadow-xl border-2 border-primary">
+                    <div className="card-body">
+                        <h2 className="card-title justify-center mb-4 text-2xl text-primary">Login</h2>
+                        <form onSubmit={handleLogin} className="flex flex-col gap-4">
+                            {/* Username and Password Inputs*/}
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Username"
+                                value={username}
+                                onChange={event => setUsername(event.target.value)}
+                                className="input input-bordered input-primary border-2 border-primary w-full bg-base-100 text-primary"
+                                required
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={event => setPassword(event.target.value)}
+                                className="input input-bordered input-primary border-2 border-primary w-full bg-base-100 text-primary"
+                                required
+                            />
+                            <button type="submit" className="btn btn-primary w-full mt-2">
+                                Login
+                            </button>
+                        </form>
+                        <p className="mt-4 text-sm text-center text-primary">
+                            Don't have an account?{" "} {/*this line was added for spacing */}
+                            <Link to="/register" className="link link-primary">
+                                Register
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+       </>
     )
 }
 
